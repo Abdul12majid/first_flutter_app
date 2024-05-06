8157897e6771534bb359e81f552f7451c93df89f
Abdul12majid <yisaabdulmajid@gmail.com>
2024-05-06 15:07:02 +0100

    rearrange projects into folders and worked on base_home_page

--- lib/pages/base_homepage.dart
+++ lib/pages/base_homepage.dart
@@ -8,26 +8,106 @@ class BaseHomePage extends StatelessWidget {
   Widget build(BuildContext context){
 
   	return Scaffold(
-  		backgroundColor: Colors.blue[200],
-  		body: Padding(
-  				padding: const EdgeInsets.all(100.0),
-  				child: Column(
-  				//mainAxisAlignment: MainAxisAlignment.center,
-	  			children: [
-	  				Text(
-		  			'Welcome to your very first flutter tutorial!',
-		  			style: TextStyle(
-		  					color: Colors.white,
-		  					fontSize: 40,
-		  					fontWeight: FontWeight.bold,
-		  				), //textstyle
-		  			), //text
-	  			
-	  			], //children
-  			), //column
-  			), //padding
+		  body: Stack(
+		    children: [
+		      // Background image layer
+		      Image.asset(
+		        'lib/images/bg.jpeg',
+		        fit: BoxFit.cover,
+		        width: double.infinity, // Ensures image fills the width
+		        height: double.infinity, // Ensures image fills the height
+		      ),
 
-  		); //scaffold
+		      // Content layer positioned on top of the background
+
+		      Padding(
+		      	padding: const EdgeInsets.all(0.0),
+		      	child: Column(
+		      	//mainAxisAlignment:MainAxisAlignment.center,
+		      	
+		      	children: [
+
+		      		Padding(
+						padding: const EdgeInsets.only(top: 50.0, left: 50.0),
+						child: Image.asset(
+							'lib/images/logo2.jpg',
+							//color: Colors.white,
+							height:240,
+							//width: 200,
+						), //imageasset
+					), //padding
+
+		      		Padding(
+
+			      	padding: const EdgeInsets.only(top: 30.0, left: 50.0),
+
+		      		child: Text(
+			        	'Welcome',
+
+			        	style: TextStyle(
+			        	fontWeight: FontWeight.bold,
+			        	fontSize: 50,
+			        	color: Colors.white,
+			        	), //TextStyle
+			        ), // text
+
+			        ), // paddding
+
+			        Padding(
+
+			      	padding: const EdgeInsets.only(top: 30.0, left: 50.0),
+
+		      		child: Text(
+			        	'To your first Flutter Project !!!',
+
+			        	style: TextStyle(
+			        	fontWeight: FontWeight.bold,
+			        	fontSize: 30,
+			        	color: Colors.white,
+			        	), //TextStyle
+			        ), // text
+
+			        ), // paddding
+
+			        const SizedBox(height: 88),
+
+
+			        Padding(
+
+			      	padding: const EdgeInsets.only(top: 30.0, left: 50.0),
+
+		      		child: ElevatedButton(
+						onPressed:() {
+							Navigator.pushNamed(context, '/firstpage');
+							},
+						style: ElevatedButton.styleFrom(
+    					backgroundColor: Colors.blue,
+						), //style
+						child: Text(
+							"Click to Start",
+							style: TextStyle(
+								fontWeight: FontWeight.bold,
+			        			fontSize: 30,
+								color: Colors.white,
+								),
+							),
+						), // button
+
+			        ), // paddding
+
+			        ],
+			      
+		      	), //column
+
+			), //padding,
+
+
+		      
+
+		    ],
+		  ), //stack
+		); //scaffold
+  	
 
 
   }
